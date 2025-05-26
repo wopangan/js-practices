c97e9482c048c246819068b7b6a143a701d3999d
wopangan <pug@users.noreply.github.com>
2025-05-26 19:17:02 +0800

    area of circle

---
+++ functions-return/task5.js
@@ -0,0 +1,11 @@
+// Write a function that returns the area of a circle with radius r.
+
+function circleArea(r) {
+	return (Math.PI * r * r);
+}
+
+// or
+
+const circleArea = (r) => Math.PI * r * r;
+
+console.log(circleArea(2));
