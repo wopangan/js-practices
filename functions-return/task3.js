9d6ec497b930c46286ef205d09309197763be73f
wopangan <pug@users.noreply.github.com>
2025-05-26 19:12:03 +0800

    greeting a person

---
+++ functions-return/task3.js
@@ -0,0 +1,15 @@
+// Write a function that returns the string "Hello, <name>!"
+
+
+function greet(name) {
+	return 'Hello, ' + name;
+}
+
+// or
+
+const greet = function(name) {
+	return 'Hello, ' + name;
+}
+
+console.log(greet('Yuji'));
+console.log(greet('Maxwell'));
