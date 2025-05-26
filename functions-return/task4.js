5593228c41626b24c344cdda068bcf75361faf74
wopangan <pug@users.noreply.github.com>
2025-05-26 19:15:25 +0800

    maximum of the three

---
+++ functions-return/task4.js
@@ -0,0 +1,22 @@
+// Write a function that returns the largest of the three numbers;
+
+function maxOfThree(a,b,c) {
+	return Math.max(a,b,c);
+}
+
+// or
+
+const maxOfThree = function(a,b,c) {
+	return Math.max(a,b,c);
+}
+
+// or
+
+const maxOfThree = (a,b,c) => {
+	return Math.max(a,b,c);
+}
+
+console.log(maxOfThree(500,50,5,5000);
+
+
+
