c0fbb56b4d2947f75d05385eb7bc6d235f51d302
wopangan <webster_pangan@yahoo.com>
2025-06-23 22:23:19 +0800

    tests only

--- js-async-http/js-Promises/promisesTest.js
+++ js-async-http/js-Promises/promisesTest.js
@@ -20,8 +20,8 @@ function getUserInfo(userId) {
   return new Promise((resolve) => {
     setTimeout(() => {
       const userInfo = {
-        1: {name: "John Doe", email: "johndoe@example.com"},
-        2: {name: "Jane Smith", email: "janesmith@example.com"}
+        1: { name: "John Doe", email: "johndoe@example.com" },
+        2: { name: "Jane Smith", email: "janesmith@example.com" },
       };
       resolve(userInfo[userId]); // remove userId to show all the user info
     }, 1000);
@@ -31,22 +31,22 @@ function getUserInfo(userId) {
 function getUserPosts(userId) {
   return new Promise((resolve) => {
     const userPosts = {
-      1: ['Post 1', 'Post 2', 'Post 3'],
-      2: ["Jane's Post 1", "Jane's Post 2"]
-    }
+      1: ["Post 1", "Post 2", "Post 3"],
+      2: ["Jane's Post 1", "Jane's Post 2"],
+    };
     resolve(userPosts[userId]); // remove [userId] to show all the user posts.
-  }, 1500)
+  }, 1500);
 }
 
 // Using Promises to simplify the code
 getUserInfo(1)
   .then((userInfo) => {
-    console.log('User Info:', userInfo);
+    console.log("User Info:", userInfo);
     return getUserPosts(1);
   })
   .then((userPosts) => {
-    console.log('User Posts:', userPosts);
+    console.log("User Posts:", userPosts);
   })
   .catch((error) => {
-    console.error('An error occurred', error);
+    console.error("An error occurred", error);
   });
